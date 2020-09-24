200bb448aab146e8347ef377985be9e6355cd658
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2020-09-24 19:45:25 +0100

    feat: Articles section done

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-const Article = () => {
+const Article = ({img, alt}) => {
   return (
     <div className='article-container'>
-      <img src="" alt=""/>
-      <div className="article-author">
-        By Claire Robinson
+      <img src={img} alt={alt}/>
+      <div className="article-body">
+        <div className="article-author">
+          By Claire Robinson
+        </div>
+        <h4>
+          Receive money in any currency with no fees
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sit vitae vel nostrum quibusdam cum fuga asperiores, quia rem quae itaque culpa nisi eaque incidunt maxime labore illo inventore. Neque?
+        </p>
       </div>
-      <h4>
-        Receive money in any currency with no fees
-      </h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sit vitae vel nostrum quibusdam cum fuga asperiores, quia rem quae itaque culpa nisi eaque incidunt maxime labore illo inventore. Neque?
-      </p>
     </div>
   )
 }
--- src/components/LatestArticles.js
+++ src/components/LatestArticles.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Container from './Container'
 import Article from './Article/Article'
+import ImageConfetti from './Article/image-confetti.jpg'
+import ImageCurrency from './Article/image-currency.jpg'
+import ImagePlane from './Article/image-plane.jpg'
+import ImageRestaurant from './Article/image-restaurant.jpg'
 
 const LatestArticles = () => {
   return (
     <Container className="latest-articles-container">
       <h1>Latest Articles</h1>
-      <Article />
-      <Article />
+      <div className="latest-articles-articles">
+        <Article img={ImageCurrency} alt='Article image of currency'/>
+        <Article img={ImageRestaurant} alt='Article image of a restaurant'/>
+        <Article img={ImagePlane} alt='Article image of a plane'/>
+        <Article img={ImageConfetti} alt='Article image of a confetti'/> 
+      </div>
     </Container>
   )
 }
